f0a4aee0fe5da9dfee525adfd73ed9eb0734222d
bones <ben@properdesign.co.uk>
2014-09-19 16:43:29 +0100

    header.php

---
+++ header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+?>
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 9]>  <html class="no-js" <?php language_attributes(); ?>><![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head id="<?php echo of_get_option('meta_headid'); ?>" data-template-set="html5-reset-wordpress-theme">
+	
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="title" content="<?php wp_title( '|', true, 'right' ); ?>">
+	<meta name="description" content="<?php bloginfo('description'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">	
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	
+	<?php get_template_part( 'head' , 'options' ); ?>
+
+	<?php if (is_search()) echo '<meta name="robots" content="noindex, nofollow" />';?>
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+	<div id="wrapper">
+		<header id="header" role="banner">
+			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		</header>
+
+		<nav id="nav" role="navigation">
+			<?php wp_nav_menu( array('menu' => 'primary') ); ?>
+		</nav>
