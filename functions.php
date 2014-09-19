d37a8df0070b33d5cf6ab1882403625edba0b9bd
TheHeat <marc@marcheatleydesign.com>
2014-09-19 16:17:41 +0100

    Stops WP adding 10px to the width of caption divs

--- functions.php
+++ functions.php
@@ -132,4 +132,21 @@
 		);
 	}
 
+// Override img caption shortcode to stop WP adding 10px to .wp-caption divs.
+add_filter('img_caption_shortcode', 'fix_img_caption_shortcode', 10, 3);
+
+function fix_img_caption_shortcode($val, $attr, $content = null) {
+    extract(shortcode_atts(array(
+        'id'    => '',
+        'align' => '',
+        'width' => '',
+        'caption' => ''
+    ), $attr));
+
+    if ( 1 > (int) $width || empty($caption) ) return $val;
+
+    return '<div id="' . $id . '" class="wp-caption ' . esc_attr($align) . '" style="width: ' . (0 + (int) $width) . 'px">' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
+
 ?>
+
