1e3f7497dd3cecd0d6f05e704234037e299c53b3
TheHeat <marc@properdesign.rs>
2014-11-18 09:57:42 +0000

    Adds full menu options in header.php

--- header.php
+++ header.php
@@ -39,5 +39,25 @@
 		</header>
 
 		<nav id="nav" role="navigation">
-			<?php wp_nav_menu( array('menu' => 'primary') ); ?>
+
+		<?php 	$args = array(
+					'theme_location' => 'primary',
+					'menu' => '',
+					'container' => 'nav',
+					'container_class' => 'menu-{menu-slug}-container',
+					'container_id' => '',
+					'menu_class' => 'menu',
+					'menu_id' => '',
+					'echo' => true,
+					'fallback_cb' => 'wp_page_menu',
+					'before' => '',
+					'after' => '',
+					'link_before' => '',
+					'link_after' => '',
+					'items_wrap' => '<ul id = "%1$s" class = "%2$s">%3$s</ul>',
+					'depth' => 0,
+					'walker' => ''
+				);
+				wp_nav_menu( $args ); ?>
+				
 		</nav>
