4bff7a42c2a7b3cfb5ba58e1fa553be7adaaa18d
TheHeat <marc@properdesign.rs>
2015-01-28 11:51:10 +0000

    More purge - fix functions.php
    Signed-off-by: TheHeat <marc@properdesign.rs>

--- functions.php
+++ functions.php
@@ -5,16 +5,10 @@
  * @since Proper Bear 1.0
  */
 
-// Options Framework (https://github.com/devinsays/options-framework-plugin)
-if ( !function_exists( 'optionsframework_init' ) ) {
-	define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/_/inc/' );
-	require_once dirname( __FILE__ ) . '/_/inc/options-framework.php';
-}
 
 // Theme Setup (based on twentythirteen: http://make.wordpress.org/core/tag/twentythirteen/)
 function proper_bear_setup() {
 	load_theme_textdomain( 'proper_bear', get_template_directory() . '/languages' );
-	add_theme_support( 'automatic-feed-links' );
 	register_nav_menu( 'primary', __( 'Navigation Menu', 'proper_bear' ) );
 	add_theme_support( 'post-thumbnails' );
 	// Enable support for HTML5 markup.
@@ -32,9 +26,6 @@ function proper_bear_scripts_styles() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
 		wp_enqueue_script( 'comment-reply' );
 
-	// Modernizr
-	// This is an un-minified, complete version of Modernizr. Before you move to production, you should generate a custom build that only has the detects you need.
-	wp_enqueue_script( 'proper_bear-modernizr', get_template_directory_uri() . '/_/js/modernizr-2.8.0.dev.js' );
 
 }
 add_action( 'wp_enqueue_scripts', 'proper_bear_scripts_styles' );
@@ -62,19 +53,7 @@ function proper_bear_wp_title( $title, $sep ) {
 }
 add_filter( 'wp_title', 'proper_bear_wp_title', 10, 2 );
 
-// Load jQuery
-if ( !function_exists( 'core_mods' ) ) {
-	function core_mods() {
-		if ( !is_admin() ) {
-			wp_deregister_script( 'jquery' );
-			wp_register_script( 'jquery', ( "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ), false);
-			wp_enqueue_script( 'jquery' );
-		}
-	}
-	add_action( 'wp_enqueue_scripts', 'core_mods' );
-}
-
-//Clean up the <head>	
+//Clean up the <head>
 function removeHeadLinks() {
    	remove_action('wp_head', 'rsd_link');
    	remove_action('wp_head', 'wlwmanifest_link');
@@ -89,7 +68,7 @@ register_nav_menu( 'primary', __( 'Navigation Menu', 'proper_bear' ) );
 
 // Navigation - update coming from twentythirteen
 function post_navigation() {
-	echo '<div class="navigation">';
+	echo '<div class="posts-navigation">';
 	echo '	<div class="next-posts">'.get_next_posts_link('&laquo; Older Entries').'</div>';
 	echo '	<div class="prev-posts">'.get_previous_posts_link('Newer Entries &raquo;').'</div>';
 	echo '</div>';
