f03b6e516c86f105f4ab7f8228d80cfbf40aa5d4
Andrew Shankie <andrew@properdesign.rs>
2015-09-18 13:09:04 +0100

    Adds and opening HTML tag

--- header.php
+++ header.php
@@ -6,12 +6,15 @@
  */
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 9]>  <html class="no-js" <?php language_attributes(); ?>><![endif]-->
-<!-- the "no-js" class is for Modernizr. -->
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
 
 <head>
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
