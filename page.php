efa481e69196b6dffe2bc8d38216753bb16d2763
TheHeat <marc@properdesign.rs>
2015-10-06 22:56:58 +0100

    no posted date on pages

--- page.php
+++ page.php
@@ -11,8 +11,7 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<main <?php post_class(); ?> id="page-<?php the_ID(); ?>">
-			<h2><?php the_title(); ?></h2>
-			<?php posted_on(); ?>
+			<?php the_title('<h1>', '</h1>'); ?>
 			<?php the_content(); ?>
 			<?php wp_link_pages(array('before' => __('Pages: ','properbear'), 'next_or_number' => 'number')); ?>
 			<?php edit_post_link(__('Edit this entry','properbear'), '<p>', '</p>'); ?>
