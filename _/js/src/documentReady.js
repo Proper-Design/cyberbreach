765840eb5ee1e3f81f5e1e3fb14d0b4f438dcb9f
bones <ben@properdesign.rs>
2016-02-11 17:55:45 +0000

    Adds cookie message

--- _/js/src/documentReady.js
+++ _/js/src/documentReady.js
@@ -17,6 +17,26 @@
 		$('.site-nav-wrapper').squishMenu();
 		$('.site-content-wrapper').fitVids();
 
+		/* EU Cookie Law */
+		/* Uncomment to add meesage
+		/* TODO - CSS :)
+		if ( 'set' !== $.cookie( 'cookie-pop' ) ) {
+		  
+		  $('body').prepend(
+		    '<div class="cookie-pop site-notice"><svg class="site-notice-icon"><use xlink:href="#notice"></use></svg><span class="cookie-law">Our site uses Cookies</span><button class="button" id="accept-cookie">OK?</button></div>'
+		  );
+
+		  $( '#accept-cookie' ).click(function () {
+
+		    $.cookie( 'cookie-pop', 'set' );
+		    $( '.cookie-pop' ).remove();
+
+		  });
+		
+		}
+
+		*/
+
 	});
 
 }(window.jQuery || window.$));
