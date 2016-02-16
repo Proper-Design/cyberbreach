fcc5bea75671edb016785b1a6f8353043566db7a
bones5 <ben@properdesign.rs>
2016-02-16 10:03:22 +0000

    Removes field sizes

--- _/inc/php/proper-email-form.php
+++ _/inc/php/proper-email-form.php
@@ -19,7 +19,6 @@ function proper_email_form() {
 				name="sender-name" 
 				pattern="[a-zA-Z0-9 ]+" 
 				value="<?php isset( $_POST["sender-name"] ) ? esc_attr( $_POST["sender-name"] ) : '' ?>" 
-				size="40" 
 			/>
 		</div>
 
@@ -29,7 +28,6 @@ function proper_email_form() {
 				type="email" 
 				name="sender-email" 
 				value="<?php echo ( isset( $_POST["sender-email"] ) ? esc_attr( $_POST["sender-email"] ) : '' ) ?>" 
-				size="40" 
 			/>
 		</div>
 
