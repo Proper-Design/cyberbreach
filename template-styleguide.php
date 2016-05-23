66c866a9d34e466bd2b6a0bf021a444332e98144
TheHeat <marc@properdesign.rs>
2016-05-23 20:25:08 +0100

    Adds dynamic styleguide navbar
    Displays links to all pages using the styleguide template

--- template-styleguide.php
+++ template-styleguide.php
@@ -11,8 +11,9 @@
 
  <div class="site-content">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <?php get_template_part('styleguide', 'nav'); ?>
 
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<main <?php post_class(); ?> id="page-<?php the_ID(); ?>">
 			<?php the_title('<h1>', '</h1>'); ?>
       <?php the_content(); ?>
