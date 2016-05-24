c32ae403319e8b0ed6b9561d885a6da90ce43a5c
propergareth <gareth@properdesign.rs>
2016-05-24 11:20:28 +0100

    Adds prototype styles and basic template

---
+++ styleguide-prototype.php
@@ -0,0 +1,11 @@
+<section class="sg-section">
+  <figure class="prototype-device iphone6">
+    <video class="prototype-video" src="<?php bloginfo('template_directory'); ?>/styleguide-iphone6.mov" controls></video>
+  </figure>
+</section>
+
+<section class="sg-section">
+  <figure class="prototype-device ipadair2">
+    <video class="prototype-video" src="<?php bloginfo('template_directory'); ?>/styleguide-iphone6.mov" controls></video>
+  </figure>
+</section>
