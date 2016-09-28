19e3bd22176e2e238cbefce97447fdf8ba69a563
Shankie <andrew@properdesign.rs>
2016-09-28 15:17:18 +0100

    Tidies up a bit…

--- _/js/src/index.js
+++ _/js/src/index.js
@@ -1,4 +1,4 @@
-import documentready from './documentready'
+import documentReady from './documentReady'
 // import styles from './index.scss'; // Use me if you want your JS to import any styles directly. Probably shouldn't be used in WP themes, but might be useful in SPAs.
 
 // Use someting like this to get started with a React app
