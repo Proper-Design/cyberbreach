99f3ebe622d34364ea7ae0e25a4c255977d6c0d7
Andrew Shankie <andrew@properdesign.co.uk>
2019-05-28 14:17:33 +0100

    Corrects header nav to align with Squish

--- header-nav.php
+++ header-nav.php
@@ -3,29 +3,29 @@
 // The primary menu as defined in functions.php
 
 ?>
-<div class="siteNav-wrapper">
+<div id="siteNav-wrapper" class="siteNav-wrapper">
 	<nav class="siteNav" role="navigation">
-	
+
 		<?php
-			$toggle = '<div class="menuToggle">' . __('Menu', 'properbear') . '</div>';
-			// Limits the menu to one level by default.
-			$args = array(
-				'theme_location' => 'primary',
-				'container' => false,
-				'container_id' => '',
-				'menu_class' => 'menu',
-				'menu_id' => '',
-				'echo' => true,
-				'fallback_cb' => '',
-				'before' => '',
-				'after' => '',
-				'link_before' => '',
-				'link_after' => '',
-				'items_wrap' => $toggle . '<ul id = "%1$s" class = "%2$s">%3$s</ul>',
-				'depth' => 1,
-				'walker' => ''
-			);
-			wp_nav_menu( $args ); ?>
+		$toggle = '<div id="siteNav-toggle" class="siteNav-toggle">' . __('Menu', 'properbear') . '</div>';
+		// Limits the menu to one level by default.
+		$args = array(
+			'theme_location' => 'primary',
+			'container' => false,
+			'container_id' => '',
+			'menu_class' => 'menu',
+			'menu_id' => '',
+			'echo' => true,
+			'fallback_cb' => '',
+			'before' => '',
+			'after' => '',
+			'link_before' => '',
+			'link_after' => '',
+			'items_wrap' => $toggle . '<ul id = "%1$s" class = "%2$s">%3$s</ul>',
+			'depth' => 1,
+			'walker' => ''
+		);
+		wp_nav_menu($args); ?>
 
 	</nav>
 </div>
