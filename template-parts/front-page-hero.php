b9715604d8e44013c8e238f19c5003691aabc658
Marc Heatley <marc@properdesign.co.uk>
2024-06-20 16:17:03 +0100

    adds video to the front page

--- template-parts/front-page-hero.php
+++ template-parts/front-page-hero.php
@@ -1,6 +1,16 @@
 		<main <?php post_class('frontPage-hero'); ?> id="page-<?php the_ID(); ?>">
 
-		<?php the_post_thumbnail('hero-small', array('class'=>"frontPage-hero-image" ));?>
+
+
+  <iframe
+	autoplay="true"
+		class="frontPage-hero-video"
+    src="https://customer-ilbdw2ocye3963e5.cloudflarestream.com/876564f565cffd56deb2a362ccfa1e96/iframe?autoplay=true&muted=true&poster=https%3A%2F%2Fcustomer-ilbdw2ocye3963e5.cloudflarestream.com%2F876564f565cffd56deb2a362ccfa1e96%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+    loading="lazy"
+    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+    allowfullscreen="true"
+
+  ></iframe>
 		
 		<div class="frontPage-hero-content">
 			<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
